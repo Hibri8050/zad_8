4a54663bcade701ff57154f04ffde232d9d0f744
Dmytro Datsenko <ddatsen1@stu.vistula.edu.pl>
2026-05-13 17:52:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,10 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '77384.html'));
+});
 
 app.post('/save-form', (req, res) => {
     const formData = req.body;
